ffb3bcf55698d44731539707b03dbe43c389a5f3
sanzhar-white <sanzzzsanzhar@gmail.com>
2025-06-27 13:50:42 +0500

    fix: resolve theme handling issue

--- lib/features/home/presentation/widgets/filter_show_bottom.dart
+++ lib/features/home/presentation/widgets/filter_show_bottom.dart
@@ -45,208 +45,224 @@ Future<SearchModel?> showCategoryFilterBottomSheet({
   await showModalBottomSheet(
     isScrollControlled: true,
     context: context,
-    backgroundColor: colorScheme.surface,
+    backgroundColor: Theme.of(context).colorScheme.surface,
     shape: RoundedRectangleBorder(borderRadius: radius),
     constraints: BoxConstraints(
       maxHeight: MediaQuery.of(context).size.height * 0.7,
     ),
     builder: (context) {
-      return StatefulBuilder(
-        builder: (context, setState) {
-          return Padding(
-            padding: EdgeInsets.only(
-              left: 16,
-              right: 16,
-              bottom: MediaQuery.of(context).viewInsets.bottom + 16,
-              top: 16,
-            ),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      return Builder(
+        builder: (bottomSheetContext) {
+          final colorScheme = Theme.of(bottomSheetContext).colorScheme;
+          final radius = ResponsiveRadius.screenBased(bottomSheetContext);
+          return StatefulBuilder(
+            builder: (context, setState) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+                  top: 16,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        S.of(context)!.filter,
-                        style: contrastBoldL(context),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            S.of(context)!.filter,
+                            style: contrastBoldL(context),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                selectedCategories.clear();
+                                selectedRegion = '';
+                                selectedCity = '';
+                                selectedSorting = 0;
+                                priceFromController.clear();
+                                priceToController.clear();
+                              });
+                            },
+                            child: Text(
+                              S.of(context)!.reset,
+                              style: TextStyle(color: colorScheme.primary),
+                            ),
+                          ),
+                        ],
                       ),
-                      TextButton(
-                        onPressed: () {
+                      const SizedBox(height: 20),
+                      Text(
+                        S.of(context)!.category,
+                        style: contrastBoldM(context),
+                      ),
+                      const SizedBox(height: 10),
+                      Wrap(
+                        spacing: 8,
+                        runSpacing: 8,
+                        children:
+                            categories.expand((category) {
+                              return category.ids.map((categoryId) {
+                                final isSelected = selectedCategories.contains(
+                                  categoryId,
+                                );
+
+                                final categoryName =
+                                    getLocalizedNameForCategoryId(
+                                      category,
+                                      context,
+                                      categoryId,
+                                    );
+
+                                return FilterChip(
+                                  label: Text(
+                                    categoryName,
+                                    style:
+                                        isSelected
+                                            ? overGreenM(context)
+                                            : contrastM(context),
+                                  ),
+                                  selected: isSelected,
+                                  onSelected: (selected) {
+                                    setState(() {
+                                      if (selected) {
+                                        selectedCategories.add(categoryId);
+                                      } else {
+                                        selectedCategories.remove(categoryId);
+                                      }
+                                    });
+                                  },
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: radius,
+                                    side: const BorderSide(
+                                      color: Colors.transparent,
+                                      width: 0,
+                                    ),
+                                  ),
+                                  backgroundColor: colorScheme.onSurface,
+                                  selectedColor: colorScheme.primary,
+                                  checkmarkColor: colorScheme.surface,
+                                );
+                              });
+                            }).toList(),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(S.of(context)!.price, style: contrastBoldM(context)),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: CustomTextField(
+                              style: contrastBoldL(context),
+                              controller: priceFromController,
+                              hintText: S.of(context)!.from,
+                              keyboardType: TextInputType.number,
+                              theme: colorScheme,
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: CustomTextField(
+                              style: contrastBoldL(context),
+                              controller: priceToController,
+                              hintText: S.of(context)!.to,
+                              keyboardType: TextInputType.number,
+                              theme: colorScheme,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        S.of(context)!.location,
+                        style: contrastBoldM(context),
+                      ),
+                      const SizedBox(height: 10),
+                      LocationPicker(
+                        locations: regions,
+                        region: selectedRegion,
+                        district: selectedCity,
+                        regionLabel: S.of(context)!.region,
+                        districtLabel: S.of(context)!.district,
+                        onRegionChanged: (region) {
                           setState(() {
-                            selectedCategories.clear();
-                            selectedRegion = '';
+                            selectedRegion = region;
                             selectedCity = '';
-                            selectedSorting = 0;
-                            priceFromController.clear();
-                            priceToController.clear();
                           });
                         },
-                        child: Text(
-                          S.of(context)!.reset,
-                          style: TextStyle(color: colorScheme.primary),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Text(S.of(context)!.category, style: contrastBoldM(context)),
-                  const SizedBox(height: 10),
-                  Wrap(
-                    spacing: 8,
-                    runSpacing: 8,
-                    children:
-                        categories.expand((category) {
-                          return category.ids.map((categoryId) {
-                            final isSelected = selectedCategories.contains(
-                              categoryId,
-                            );
-
-                            final categoryName = getLocalizedNameForCategoryId(
-                              category,
-                              context,
-                              categoryId,
-                            );
-
-                            return FilterChip(
-                              label: Text(
-                                categoryName,
-                                style:
-                                    isSelected
-                                        ? overGreenM(context)
-                                        : contrastM(context),
-                              ),
-                              selected: isSelected,
-                              onSelected: (selected) {
-                                setState(() {
-                                  if (selected) {
-                                    selectedCategories.add(categoryId);
-                                  } else {
-                                    selectedCategories.remove(categoryId);
-                                  }
-                                });
-                              },
-                              shape: RoundedRectangleBorder(
-                                borderRadius: radius,
-                                side: const BorderSide(
-                                  color: Colors.transparent,
-                                  width: 0,
-                                ),
-                              ),
-                              backgroundColor: colorScheme.onSurface,
-                              selectedColor: colorScheme.primary,
-                              checkmarkColor: colorScheme.surface,
-                            );
+                        onDistrictChanged: (district) {
+                          setState(() {
+                            selectedCity = district;
                           });
-                        }).toList(),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(S.of(context)!.price, style: contrastBoldM(context)),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: CustomTextField(
-                          style: contrastBoldL(context),
-                          controller: priceFromController,
-                          hintText: S.of(context)!.from,
-                          keyboardType: TextInputType.number,
-                          theme: colorScheme,
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      Text(S.of(context)!.sort, style: contrastBoldM(context)),
+                      const SizedBox(height: 10),
+                      DropdownButtonFormField<int>(
+                        value: selectedSorting,
+                        items:
+                            sortingOptions.entries.map((entry) {
+                              return DropdownMenuItem(
+                                value: entry.key,
+                                child: Text(
+                                  entry.value,
+                                  style: contrastM(context),
+                                ),
+                              );
+                            }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedSorting = value!;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: colorScheme.onSurface,
+                          border: OutlineInputBorder(
+                            borderRadius: radius,
+                            borderSide: BorderSide.none,
+                          ),
                         ),
                       ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: CustomTextField(
-                          style: contrastBoldL(context),
-                          controller: priceToController,
-                          hintText: S.of(context)!.to,
-                          keyboardType: TextInputType.number,
-                          theme: colorScheme,
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            result = SearchModel(
+                              categories:
+                                  selectedCategories.isNotEmpty
+                                      ? selectedCategories.toList()
+                                      : null,
+                              priceFrom: int.tryParse(priceFromController.text),
+                              priceTo: int.tryParse(priceToController.text),
+                              region: int.tryParse(selectedRegion),
+                              district: int.tryParse(selectedCity),
+                              sortBy: selectedSorting,
+                            );
+                            onApply(result);
+                            context.pop();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: colorScheme.primary,
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(borderRadius: radius),
+                          ),
+                          child: Text(
+                            S.of(context)!.apply,
+                            style: TextStyle(color: colorScheme.onPrimary),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 20),
-                  Text(S.of(context)!.location, style: contrastBoldM(context)),
-                  const SizedBox(height: 10),
-                  LocationPicker(
-                    locations: regions,
-                    region: selectedRegion,
-                    district: selectedCity,
-                    regionLabel: S.of(context)!.region,
-                    districtLabel: S.of(context)!.district,
-                    onRegionChanged: (region) {
-                      setState(() {
-                        selectedRegion = region;
-                        selectedCity = '';
-                      });
-                    },
-                    onDistrictChanged: (district) {
-                      setState(() {
-                        selectedCity = district;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  Text(S.of(context)!.sort, style: contrastBoldM(context)),
-                  const SizedBox(height: 10),
-                  DropdownButtonFormField<int>(
-                    value: selectedSorting,
-                    items:
-                        sortingOptions.entries.map((entry) {
-                          return DropdownMenuItem(
-                            value: entry.key,
-                            child: Text(entry.value, style: contrastM(context)),
-                          );
-                        }).toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        selectedSorting = value!;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: colorScheme.onSurface,
-                      border: OutlineInputBorder(
-                        borderRadius: radius,
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        result = SearchModel(
-                          categories:
-                              selectedCategories.isNotEmpty
-                                  ? selectedCategories.toList()
-                                  : null,
-                          priceFrom: int.tryParse(priceFromController.text),
-                          priceTo: int.tryParse(priceToController.text),
-                          region: int.tryParse(selectedRegion),
-                          district: int.tryParse(selectedCity),
-                          sortBy: selectedSorting,
-                        );
-                        onApply(result);
-                        context.pop();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: colorScheme.primary,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(borderRadius: radius),
-                      ),
-                      child: Text(
-                        S.of(context)!.apply,
-                        style: TextStyle(color: colorScheme.onPrimary),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           );
         },
       );
